e175df2ad49541ce749ee76a46fa71272db79a5d
KenyiChino <kenny_9_7@hotmail.com>
2026-07-25 23:31:17 -0500

    Desarrollo del metodo ,get,post and put

---
+++ Enquiry.api/Enquiry.api/Controllers/ServiceMasterController.cs
@@ -0,0 +1,49 @@
+﻿using Enquiry.api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Enquiry.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceMasterController : ControllerBase
+    {
+        private readonly EnquiryDbContext _context;
+
+        public ServiceMasterController(EnquiryDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Services> getAllService()
+        {
+            var services = _context.Services.ToList();
+            return services;
+        }
+
+        [HttpPost]
+        public IActionResult AddNewService(Services obj)
+        {
+            _context.Services.Add(obj);
+            _context.SaveChanges();
+            return Created("Service Created Success", obj);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateService(int serviceId, Services obj)
+        {
+            var oldServiceData= _context.Services.SingleOrDefault(x => x.ServiceId == serviceId);
+            if (oldServiceData != null)
+            {
+                oldServiceData.ServiceName = obj.ServiceName;
+                oldServiceData.IsActive = obj.IsActive;
+                _context.SaveChanges();
+                return Ok("Service Update Success");
+            } else
+            {
+                return NotFound("Service Not Found with id " + serviceId);
+            }
+        }
+    }
+}
---
+++ Enquiry.api/Enquiry.api/Models/Services.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Enquiry.api.Models
+{
+    public class Services
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ServiceId { get; set; }
+        public string ServiceName { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public bool IsActive { get; set; }
+        public double Rate { get; set; }
+    }
+}
